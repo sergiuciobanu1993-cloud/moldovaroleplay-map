a220d6d49d6ff9c786c1b0d41fd9bad41857f277
sergiuciobanu1993-cloud <sergiuciobanu1993@gmail.com>
2026-07-18 21:10:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const map = L.map('map',{
-crs:L.CRS.Simple,
-minZoom:-2
+const map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -2
 });
 
-const bounds=[
-[0,0],
-[1000,1000]
+const bounds = [
+    [0, 0],
+    [1000, 1000]
 ];
 
-L.imageOverlay('map.png',bounds).addTo(map);
+L.imageOverlay('image.png', bounds).addTo(map);
 
 map.fitBounds(bounds);
